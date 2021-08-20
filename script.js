e905ee2bd5cfd850dfd3058f10a129bbad0c32de
Leonel Mendoza <81709781+leomendozaesp@users.noreply.github.com>
2021-08-20 14:37:36 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('ready')
+console.log('ready');
+
+function near() {
+    alert("Proximamente")
+};
+
+function color() {
+    document.getElementById('corp').style.background = black;
+};
+
+function white() {
+    x = document.getElementById('corp');
+    x.style.background = white;
+}
