a335f23d277e160c39e904f413bf1c03beae596d
Leonel Mendoza <81709781+leomendozaesp@users.noreply.github.com>
2021-08-25 19:56:34 -0300

    update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log('ready');
 function advert() {
     alert("Proximamente..");
 };
+
+function loading() {
+    let x = document.GetElementById('price');
+    let value = "$00"
+    x.innerHTML = value;
+}
