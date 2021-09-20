f8f45a75d4f81d00c5daf880af0775c7b7fecf5a
Leonel Mendoza <81709781+leomendozaesp@users.noreply.github.com>
2021-09-20 20:15:35 -0300

    update #script.js

--- #script.js
+++ #script.js
@@ -1,1 +1,16 @@
 document.getElementById('thumb-logo').style.display = 'none';
+var myObject = {
+        sayHello: function() {
+            console.log("hello")
+    },
+    myName : 'Leonel'
+};
+myObject.sayHello();
+
+var Lavandina = "Lavandina";
+
+var yes = true;
+var no = false;
+
+document.getElementByValue()
+document.getElementsByTagName("h1").innerHTML = Lavandina; 
