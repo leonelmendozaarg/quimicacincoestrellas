deda0ef2053a76b9fab346176ab9e5d6a2abbe16
Leonel Mendoza <81709781+leomendozaesp@users.noreply.github.com>
2021-11-29 00:44:21 -0300

    update script.js (the worst)

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-console.log('ready');
-
-function advert() {
-    alert("Proximamente..");
+console.log("Load");
+//document.getElementById('contenido').style.display = 'none';
+function openNav() {
+  document.getElementById('contenido').style.display = '', 100;
 };
-
-function imprimir() {
-    document.getElementById('footer').style.display = "none"; 
-    window.print();
+if (window.scrollY >= 50) {
+document.getElementsById('top').style.display = 'block';
+console.log(window.scrollY);
+} else if (window.scrollY <= 50) {
+  document.getElementById('top').style.display = 'none';
+  console.log(window.scrollY);
 }
