eb097b72f4f6a163f05080d97be3ce8c617aabcc
Leonel Mendoza <81709781+leomendozaesp@users.noreply.github.com>
2021-11-29 01:03:36 -0300

    update script.js

--- script.js
+++ script.js
@@ -10,3 +10,44 @@ console.log(window.scrollY);
   document.getElementById('top').style.display = 'none';
   console.log(window.scrollY);
 }
+
+         function onloading() {
+          let rangeValue = document.getElementById('range').value;
+          let x = 0;
+          let y = x * 100;
+          console.log("data is changing")
+          if (rangeValue != y) {
+            document.getElementById('contenido').style.fontSize = rangeValue + "px";
+            console.log("The fontSize is: " + rangeValue + "px");
+          } else {
+            console.log('is not');
+          }
+         };
+         document.getElementById('headerNav').style.width = "0px";
+         function openHeaderNav() {
+           document.getElementById('headerNav').style.width = "250px";
+           document.getElementById('headerNavContent').style.display = 'block';
+           console.log("Navigation open");
+         }
+         function closeHeaderNav() {
+           document.getElementById('headerNav').style.width = "0px";
+           document.getElementById('headerNavContent').style.display = 'none';
+           console.log("Navigation closed");
+         }
+         function darkMode() {
+           let rangeValue = document.getElementById('range').value;
+           console.log("Dark mode: switch working.")
+           let x = rangeValue <= 50;
+           let z = rangeValue >= 50;
+           if (x) {
+             document.body.style.background = "white";
+             document.getElementById('headerNav').style.background = "blue";
+             console.log("Switched to white mode");
+           } else if (z) {
+             document.body.style.background = "black";
+             document.getElementById('headerNav').style.background = //"#025348";
+             console.log("Switched to black mode");
+           } else {
+             console.log("Wont work.")
+           }
+         }
